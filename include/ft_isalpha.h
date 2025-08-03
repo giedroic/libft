6e849fbe76ff458fbc328f26fd7367d56a43cd23
Aleksander Giedroic <agiedroi@c1r1s4.42warsaw.pl>
2025-08-03 18:26:48 +0200

    Add and implement libc isalpha function

---
+++ include/ft_isalpha.h
@@ -0,0 +1,5 @@
+#ifndef FT_ISALPHA_H
+# define FT_ISALPHA_H
+
+int	ft_isalpha(int c);
+#endif
---
+++ libc/ft_isalpha.c
@@ -0,0 +1,17 @@
+static int	ft_islower(int c);
+static int	ft_isupper(int c);
+
+int	ft_isalpha(int c)
+{
+	return (ft_islower(c) || ft_isupper(c));
+}
+
+static int	ft_islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+static int	ft_isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
