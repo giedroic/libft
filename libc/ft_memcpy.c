998c630e9bec844a6f91adddca55303790960884
giedroic <150075381+giedroic@users.noreply.github.com>
2025-08-19 12:24:40 +0200

    Check for NULL arguments

--- libc/ft_memcpy.c
+++ libc/ft_memcpy.c
@@ -1,7 +1,10 @@
 #include "../include/ft_memcpy.h"
+#include <stdio.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (n--)
 		*((char *) dst + n) = *((char *) src + n);
 	return (dst);
