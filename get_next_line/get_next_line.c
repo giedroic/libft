2b945f55df54903409100b6833a245c57fa40d70
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-01 18:37:06 +0200

    Move back helper functions into main get_next_line source file

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,19 @@
 /*   By: agiedroi <agiedroi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 23:53:28 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/01 18:00:35 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/01 18:35:09 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include "get_next_line.h"
-#include <stddef.h>
+#include "libft.h"
+
+static char	*update_stash(char *stash);
+static char	*extract_line(char *stash);
+static char	*read_line(char *stash, int fd);
+
 
 char	*get_next_line(int fd)
 {
@@ -27,3 +34,54 @@ char	*get_next_line(int fd)
 	stash = update_stash(stash);
 	return (line);
 }
+
+static char	*update_stash(char *stash)
+{
+	char	*new_stash;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	new_stash = ft_calloc(ft_strlen(stash) - i + 1, sizeof(char));
+	if (!new_stash)
+		return (NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	free(stash);
+	return (new_stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] && stash[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+static char	*read_line(char *stash, int fd)
--- include/get_next_line.h
+++ include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agiedroi <agiedroi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 23:53:35 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/01 17:57:43 by agiedroi         ###   ########.fr       */
+/*   Updated: 2025/09/01 18:34:10 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,4 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*update_stash(char *stash);
-char	*extract_line(char *stash);
-char	*read_line(char *stash, int fd);
 #endif
