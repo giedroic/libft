a0c2c261fb3473b43d47a8387ab3df59e694af44
giedroic <150075381+giedroic@users.noreply.github.com>
2025-09-21 14:42:14 +0000

    Add function prototype

--- include/ft_putstr_fd.h
+++ include/ft_putstr_fd.h
@@ -1,13 +1,17 @@
-t
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agiedroi <agiedroi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/21 14:34:52 by agiedroi          #+#    #+#             */
-/*   Updated: 2025/09/21 14:36:15 by agiedroi         ###   ########.fr       */
+/*   Created: 2025/09/21 14:39:02 by agiedroi          #+#    #+#             */
+/*   Updated: 2025/09/21 14:41:56 by agiedroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PUTSTR_FD_H
+# define FT_PUTSTR_FD_H
 
+void	ft_putstr_fd(char *s, int fd);
+#endif
